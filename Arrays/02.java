4cd0444f9ac52e40a14c0f24ce052302d6d5d697
Aastha <aasthajasuja2020@gmail.com>
2025-11-24 17:19:46 +0530

    Sum of all elements ( GFG )

---
+++ Arrays/02.java
@@ -0,0 +1,9 @@
+ class Solution {
+    public static int largest(int[] arr) {
+        int sum-0;
+        for(int i=0;i<arr.length;i++){
+           sum+=arr[i];
+        }
+        return sum;
+    }
+} 
