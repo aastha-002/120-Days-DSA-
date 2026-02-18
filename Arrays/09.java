798093b32f624e03bdd914e754eba0557ea53ab0
Aastha <aasthajasuja2020@gmail.com>
2026-02-18 22:19:02 +0530

    Two Sum (Leetcode-1)

--- Arrays/09.java
+++ Arrays/09.java
@@ -1,21 +1,13 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int n=numbers.length;
-        int []arr=new int[2];
-        int i=0;
-        int j=n-1;
-        while(i<j){
-            int sum=numbers[i]+numbers[j];
-            if(sum<target){
-                i++;
-            }
-            else if(sum>target){
-                j--;
-            }
-            else{
-                arr[0]=i+1;
-                arr[1]=j+1;
-                break;
+    public int[] twoSum(int[] nums, int target) {
+        int [] arr=new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    arr[0]=i;
+                    arr[1]=j;
+                    break;
+                }
             }
         }
         return arr;
