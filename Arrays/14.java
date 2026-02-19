df8a4310faeabebef782b24b12acbc1fdaa0b7ab
Aastha <aasthajasuja2020@gmail.com>
2026-02-19 11:26:37 +0530

    Longest Consecutive Sequence (Leetcode-128)

---
+++ Arrays/14.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n=nums.length;
+        int longest=1;
+        if(n==0){
+            return 0;
+        }
+        HashSet<Integer> set=new HashSet<>();
+        for(int num:nums){
+            set.add(num);
+        }
+        for(int num:set){
+            if(!set.contains(num-1)){
+                int current=num;
+                int count=1;
+                while(set.contains(current+1)){
+                    current++;
+                    count++;
+                }
+                longest=Math.max(longest,count);
+            }
+        }
+        return longest;
+       
+    }
+}
